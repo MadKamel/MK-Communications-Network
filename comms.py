2928d7f92a849efc2970cb23a002510c398bed87
Eric Rustrum <rustrumek@gmail.com>
2020-11-18 23:55:52 +0000

    Updated 'comms.py' from KBot ver.

--- comms.py
+++ comms.py
@@ -7,3 +7,16 @@ def parsecmd(msg):
     cmd = spl[3][1:].split('\r\n')[0]
   
   return cmd, user
+
+def format_msg(msg):
+  outgoing = []
+  outgoing.append('discordMsg')
+  outgoing.append(msg.content)
+  outgoing.append(msg.author.name)
+  outgoing.append(msg.channel.name)
+  return '|'.join(outgoing)
+
+
+def format_packet(packet):
+  parse = packet.split('|')
+  return parse
