6cc6a23ad7047ae86e09317c3fd2b0e1d6ab1a17
Eric Rustrum <rustrumek@gmail.com>
2020-11-19 00:24:07 +0000

    Reverted 'comms.py'

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ client.connect(server, channel, nickname)
 while True:
   cmd, user = comms.parsecmd(client.get_text())
   if not cmd == None:
-    if cmd == 'ping':
+    if cmd[:4] == 'ping':
       client.send(channel, 'pong')
       print('ping from ' + user + ' ponged.')
     
-    elif cmd == 'pong':
+    elif cmd[:4] == 'pong':
       print('pong from ' + user + ' recieved.')
 
     elif cmd[:4] == 'send':
